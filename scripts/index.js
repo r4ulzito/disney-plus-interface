9cd038340333fa0181c2cd77267cc4e398e937f7
Raul de Souza <raulsouza180802@gmail.com>
2022-06-24 12:02:02 -0300

    version with navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const API_KEY = `8f048d8bf0dcc8839670fde2addd2368`;
 const API_LANGUAGE = `pt-br`;
-const BASE_URL_IMAGE = `https://image.tmdb.org/t/p/original`;
+const BASE_URL_IMAGE = {
+  original: `https://image.tmdb.org/t/p/original`,
+  small: `https://image.tmdb.org/t/p/w500`,
+};
 const LIST_MOVIES = [
   "tt2380307",
   "tt12801262",
@@ -10,13 +13,20 @@ const LIST_MOVIES = [
   "tt2948372",
   "tt2953050",
   "tt3521164",
-  "tt8097030",
 ];
 
-const moviesList = document.getElementById("movies__list");
+const moviesList = document.getElementById("movies");
 
 function getUrlMovie(movieId) {
   return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
+}
+
+function changeButtonMenu() {
+  const button = document.querySelector(".button__menu");
+  const navigation = document.querySelector(".navigation");
+
+  button.classList.toggle("active");
+  navigation.classList.toggle("active");
 }
 
 //Inicializa os dados do filme principal
@@ -24,10 +34,10 @@ function setMainMovie(movieId) {
   fetch(getUrlMovie(movieId))
     .then((response) => response.json())
     .then((data) => {
-      const app = document.getElementById("app");
-      const title = document.querySelector(".movie h1");
-      const description = document.querySelector(".movie p");
-      const info = document.querySelector(".movie span");
+      const appImage = document.querySelector(".app__image img");
+      const title = document.querySelector(".feature__movie h1");
+      const description = document.querySelector(".feature__movie p");
+      const info = document.querySelector(".feature__movie span");
       const rating = document.querySelector(".rating strong");
 
       const yearRelease = data.release_date.split("-")[0];
@@ -37,14 +47,10 @@ function setMainMovie(movieId) {
       rating.innerHTML = data.vote_average;
       info.innerHTML = `${yearRelease} - ${data.genres[0].name} - Filme`;
 
-      const image = BASE_URL_IMAGE.concat(data.backdrop_path);
+      const image = BASE_URL_IMAGE.original.concat(data.backdrop_path);
+      appImage.setAttribute("src", image);
 
-      app.style.backgroundImage = `linear-gradient(
-      90.18deg,
-      rgb(13, 22, 46, 0.7) 23.21%,
-      rgba(13, 22, 46, 0.0001) 96.69%
-    ),
-    url("${image}")`;
+      changeButtonMenu();
     });
 }
 
@@ -56,24 +62,36 @@ function createButtonMovie(movieId) {
   return button;
 }
 
+function createImageMovie(movieImage, movieTitle) {
+  const divImageMovie = document.createElement("div");
+  divImageMovie.classList.add("movie__image");
+  const image = document.createElement("img");
+
+  image.setAttribute("src", movieImage);
+  image.setAttribute("alt", `Imagem do filme ${movieTitle}`);
+  image.setAttribute("loading", "lazy");
+
+  divImageMovie.appendChild(image);
+
+  return divImageMovie;
+}
+
 function createMovie(movieId) {
   fetch(getUrlMovie(movieId))
     .then((response) => response.json())
     .then((data) => {
       const movie = document.createElement("li");
+      movie.classList.add("movie");
+
       const genre = `<span>${data.genres[0].name}</span>`;
       const title = `<strong>${data.title}</strong>`;
 
-      const image = BASE_URL_IMAGE.concat(data.backdrop_path);
+      const image = BASE_URL_IMAGE.small.concat(data.backdrop_path);
 
       movie.innerHTML = genre + title;
       movie.appendChild(createButtonMovie(movieId));
-      movie.style.backgroundImage = `linear-gradient(
-        180deg,
-        rgba(14, 23, 47, 0.0001) 11.72%,
-        #0e172f 100%
-      ),
-      url("${image}")`;
+      movie.appendChild(createImageMovie(image, data.title));
+
       moviesList.appendChild(movie);
     });
 }
